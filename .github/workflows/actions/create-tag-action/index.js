afb9671ed3c70b371849082fd4e7beb6e447ceb6
火龙果 <shengteli@caih.com>
2025-02-10 11:09:34 +0800

    commit:test

--- .github/workflows/actions/create-tag-action/index.js
+++ .github/workflows/actions/create-tag-action/index.js
@@ -1,28 +1,27 @@
-import { execSync } from 'node:child_process';
-import { GitHub } from '@actions/github/lib/utils';
+import { getOctokit, context } from '@actions/github';
+import { Github } from '@actions/github/lib/utils';
 
 async function run() {
-  try {
-    // 获取输入参数
-    const tabName = 'test-v1.1.1';
-    
-    console.log(`Creating tag: ${tabName}`);
+    try {
+        // 获取输入参数
+        const github = getOctokit(process.env.GITHUB_TOKEN);
+        const tagName = 'test-v1.1.1';
+        const createdRelease = await github.rest.repos.createRelease({
+            owner:'owb',
+            repo:'test',
+            tag_name: tagName,
+            name: 'releaseName',
+            body: 'test',
+            draft:false,
+            prerelease:false,
+            target_commitish: commitish || context.sha,
+        });
 
-    // 配置 git 用户名和邮箱
-    execSync('git config --global user.name "GitHub Actions"');
-    execSync('git config --global user.email "github-actions[bot]@users.noreply.github.com"');
+        release = createdRelease.data;
 
-    // 创建新的标签
-    execSync(`git tag ${tabName}`);
-    console.log(`Tag created: ${tabName}`);
-
-    // 推送标签到远程仓库
-    execSync(`git push origin ${tabName}`);
-    console.log(`Tag pushed to remote: ${tabName}`);
-    
-  } catch (error) {
-    //setFailed(error.message);
-  }
+    } catch (error) {
+        //setFailed(error.message);
+    }
 }
 
 run();
