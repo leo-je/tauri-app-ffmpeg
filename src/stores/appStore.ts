3fb81b340926f14d73624c1399c3b54318f982f8
火龙果 <405192400@qq.com>
2026-03-17 16:59:44 +0800

    commit:优化界面

--- src/stores/appStore.ts
+++ src/stores/appStore.ts
@@ -1,4 +1,4 @@
-import { Store } from '@tauri-apps/plugin-store';
+import { createStore, type Store } from '@tauri-apps/plugin-store';
 import type { AppFormData, SettingForm } from '../types';
 
 let storeInstance: Store | null = null;
@@ -7,7 +7,7 @@ let initPromise: Promise<Store> | null = null;
 async function getStore(): Promise<Store> {
   if (storeInstance) return storeInstance;
   if (!initPromise) {
-    initPromise = Store.load('store.json').then(s => {
+    initPromise = createStore('store.json').then((s: Store) => {
       storeInstance = s;
       return s;
     });
